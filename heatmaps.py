40cb9f4376f6b54dca107ee3eb77a9d2ab7b8b7a
omerchor <65480691+omerchor@users.noreply.github.com>
2020-09-09 16:14:27 +0300

    Started adding fourier envelope code Heatmaps now combines files for same board size. Images are resized using scikit rather than manually

--- heatmaps.py
+++ heatmaps.py
@@ -4,73 +4,105 @@ import scipy.io as sio
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import skimage.transform
+from multiprocessing import Pool
 import analyze_stk
 # %%
-IMAGE_RESOLUTION = np.array([350,290])
 CELLS_RATIO = 19
 #%%
-files = os.listdir("num_hexbugs_stks")
+# DIRNAME = "stks"  # "num_hexbugs_stks"
+DIRNAME = "num_hexbugs_stks"
+files = os.listdir(DIRNAME)
+
+
 #%%
-results = []
-previous = ""
-for filename in files:
-    # If the file is a new number, reset the pixels matrix
-    if filename != previous and filename.replace("_2", "") != previous:
-        # Create an empty matrix for results
-        pixels = np.zeros(IMAGE_RESOLUTION // CELLS_RATIO)
-        previous = filename
-    # Otherwise, the existing pixels map will be used. Remove the last items
-    # from the results as it will be updated here
-    else:
-        results.pop()
-    
+def load_stk(filename):
     # Load .mat file
     print(filename)
-    f = sio.loadmat("num_hexbugs_stks\\" + filename)
-    stk = f['stk']
-    
+    f = sio.loadmat(os.path.join(DIRNAME, filename))
+    return f['stk']
+
+
+def find_densities(filename):
+    stk = load_stk(filename)
+
+    x_boundaries = [stk[:, 0].min(), stk[:, 0].max()]
+    y_boundaries = [stk[:, 1].min(), stk[:, 1].max()]
+    resolution = np.ceil(np.array([y_boundaries[1] - y_boundaries[0],
+                                   x_boundaries[1] - x_boundaries[0]])).astype("int")
+    pixels = np.zeros(resolution)
     # Update pixels matrix frame by frame
-    current_pixels = np.zeros(IMAGE_RESOLUTION // CELLS_RATIO)
+    current_pixels = np.zeros(resolution)
     current_frame = 1
     for entry in stk:
         # If we have advanced a frame, update the full table and reset
         if entry[4] != current_frame:
             current_frame = entry[4]
             pixels += current_pixels
-            current_pixels = np.zeros(IMAGE_RESOLUTION // CELLS_RATIO)
-        x = int(np.floor(entry[0] / CELLS_RATIO))
-        y = int(np.floor(entry[1] / CELLS_RATIO))
-        current_pixels[y,x] = 1
-        
+            # Reset the current pixels matrix for the next frame
+            current_pixels.fill(0)
+        x = np.floor(entry[0] - x_boundaries[0]).astype("int")
+        y = np.floor(entry[1] - y_boundaries[0]).astype("int")
+        current_pixels[y, x] = 1
+
     # Add last frame
     pixels += current_pixels
-    results.append(pixels)
+    return pixels, current_frame
+
+
 #%%
-# Create multiple figures for all results (two in a row - adds as many rows needed)
-num_columns = 3
-fig, axs = plt.subplots(int(np.ceil(len(results) / num_columns)), num_columns,
-                        figsize=(12, 11))
-cbar_ax = fig.add_axes([1, 0.15, 0.05, 0.7])
+if __name__ == "__main__":
+    with Pool(8) as p:
+        results = p.map(find_densities, files)
+    # results = [find_densities(f) for f in files]
+#%%
+    small_results = [skimage.transform.downscale_local_mean(res[0], (CELLS_RATIO, CELLS_RATIO)) for res in results]
 
-# Plot each result in its place
-for i, item in enumerate(results):
-    # Normalize values
-    vmin = item.min()
-    vmax = item.max()
-    item = (item - vmin)/(vmax - vmin)
+    # Join results for same board size
+    unique_filenames = list(set([f.replace("_1", "").replace("_2", "") for f in files]))
+    unique_results = []
+    for filename in unique_filenames:
+        indices = [i for i, f in enumerate(files) if f.replace("_1", "").replace("_2", "") == filename]
+        new_res = None
+        for i in indices:
+            # Normalize result by dividing by num of frames
+            if new_res is None:
+                new_res = small_results[i] / results[i][1]
+            # Add additional file with same length
+            else:
+                # Normalize new image with its num of frames
+                second_img = skimage.transform.resize(small_results[i], new_res.shape)
+                new_res += second_img / results[i][1]
 
-    # Show the heatmap
-    # vmax can be used to add a maximal value - the laser is sometimes detected in the
-    # image, making the area near the moving wall appear significantly busier
-    im = axs[i // num_columns, i % num_columns].imshow(item, interpolation='gaussian',
-                                                       cmap='inferno', vmax=0.5,
-                                                       label=i+1)
-    axs[i // num_columns, i % num_columns].set_title("Number of hexbugs: " + str(i + 1))
-    
-# Add colorbar
-cb = fig.colorbar(im, cax=cbar_ax)
-fig.suptitle("Heatmaps of hexbugs positions for different amounts of bugs", fontsize=16)
-fig.subplots_adjust(top=0.98)
-fig.tight_layout()
-fig.savefig("heatmaps.png", bbox_inches='tight')
-fig.show()
+        unique_results.append(new_res)
+
+
+    #%%
+    # Create multiple figures for all results (two in a row - adds as many rows needed)
+    num_columns = 3
+    # resimgs = [r[0] for r in results]
+    fig, axs = plt.subplots(int(np.ceil(len(unique_results) / num_columns)), num_columns,
+                            figsize=(12, 11))
+    cbar_ax = fig.add_axes([1, 0.15, 0.05, 0.7])
+
+    # Plot each result in its place
+    for i, item in enumerate(unique_results):
+        # Normalize values
+        vmin = item.min()
+        vmax = item.max()
+        item = (item - vmin)/(vmax - vmin)
+
+        # Show the heatmap
+        # vmax can be used to add a maximal value - the laser is sometimes detected in the
+        # image, making the area near the moving wall appear significantly busier
+        im = axs[i // num_columns, i % num_columns].imshow(item, interpolation='gaussian',
+                                                           cmap='inferno', vmax=0.7)
+        axs[i // num_columns, i % num_columns].set_title("Number of hexbugs: " + str(i+1))
+
+    # Add colorbar
+    cb = fig.colorbar(im, cax=cbar_ax)
+    fig.suptitle("Heatmaps of hexbug positions for different amounts of bugs", fontsize=16)
+    fig.tight_layout()
+    fig.subplots_adjust(top=0.90)
+    fig.savefig("heatmaps.png", bbox_inches='tight')
+    fig.show()
--- main.py
+++ main.py
@@ -2,73 +2,73 @@ import os.path
 import glob
 from multiprocessing import Pool
 from analyze_stk import *
-import lab
+import scipy.signal
 #%%
 # Execute whole script from here
 if __name__ == "__main__":
 # %%
-    files = glob.glob("stks/*.mat")
+    files = glob.glob("stks/*.mat")[24:25]
     infos = np.zeros(len(files))
-    lengths = [float(os.path.basename(os.path.splitext(filename)[0]).replace("_2","").replace("_1","")) for filename in files]
+    lengths = [float(os.path.basename(os.path.splitext(filename)[0]).replace("_2", "").replace("_1", "")) for filename in files]
 #%%
-    with Pool(8) as p:
+    with Pool(1) as p:
         # Analyze the files
         analysis_results = p.map(parse_file, files)
         # Find cell occupied probabilities in each file
         occupied_probabilities = p.map(get_probabilities, analysis_results)
         # Analyze frame correlations in each file
         # Result is list of [lags, values, peaks, peak_values, peak_widths]
-        correlations = p.map(autocorrelations, analysis_results)
-        slope_params = [(c[0], c[1]) for c in correlations]
-        slopes = p.starmap(first_decay_slope, slope_params)
+        # correlations = p.map(autocorrelations, analysis_results)
+        # slope_params = [(c[0], c[1]) for c in correlations]
+        # slopes = p.starmap(first_decay_slope, slope_params)
 #%%
-    plt.plot(lengths, slopes, ".")
-    plt.title("Autocorrelations decay slope (semilog) as function of board size")
-    plt.xlabel("Board length (cm)")
-    plt.ylabel("Decay semilog slope")
-    plt.show()
+    # plt.plot(lengths, slopes, ".")
+    # plt.title("Autocorrelations decay slope (semilog) as function of board size")
+    # plt.xlabel("Board length (cm)")
+    # plt.ylabel("Decay semilog slope")
+    # plt.show()
 # %%
-    blocked = [a[:, 0].astype("float") for a in analysis_results]
-    blocked = [[b - b.mean()] for b in blocked]
-    peaks = [(c[2], c[3]) for c in correlations]
-    # multi_plot("stem", len(peaks), peaks,
-    #            main_title="Correlation peaks", titles=lengths,
-    #            setp_kwargs={"xlim":[0,10000], "ylim":[0,0.2]})
-    multi_plot("acorr", len(blocked), blocked, main_title="Correlations_full",
-               titles=lengths, is_wide=True, num_columns=2,
-               setp_kwargs={"xlim": 0, "ylim": [-0.075, 0.075]},
-               maxlags=None, usevlines=True)
+#     blocked = [a[:, 0].astype("float") for a in analysis_results]
+#     blocked = [[b - b.mean()] for b in blocked]
+#     peaks = [(c[2], c[3]) for c in correlations]
+#     # multi_plot("stem", len(peaks), peaks,
+#     #            main_title="Correlation peaks", titles=lengths,
+#     #            setp_kwargs={"xlim":[0,10000], "ylim":[0,0.2]})
+#     multi_plot("acorr", len(blocked), blocked, main_title="Correlations_full",
+#                titles=lengths, is_wide=True, num_columns=2,
+#                setp_kwargs={"xlim": 0, "ylim": [-0.075, 0.075]},
+#                maxlags=None, usevlines=True)
 
 #%%
     # Distance between peaks
-    peaks = [c[2] / FPS for c in correlations]
-    peak_distances = [np.diff(p) for p in peaks]
-    average_peak_distances = [p.mean() for p in peak_distances]
-    peak_distances_stdev = [p.std() for p in peak_distances]
-    plt.errorbar(lengths, average_peak_distances, peak_distances_stdev, fmt=".")
-    plt.title("Average time difference between correlation peaks")
-    plt.xlabel("Board length (cm)")
-    plt.ylabel("Time difference (sec)")
-    plt.show()
+    # peaks = [c[2] / FPS for c in correlations]
+    # peak_distances = [np.diff(p) for p in peaks]
+    # average_peak_distances = [p.mean() for p in peak_distances]
+    # peak_distances_stdev = [p.std() for p in peak_distances]
+    # plt.errorbar(lengths, average_peak_distances, peak_distances_stdev, fmt=".")
+    # plt.title("Average time difference between correlation peaks")
+    # plt.xlabel("Board length (cm)")
+    # plt.ylabel("Time difference (sec)")
+    # plt.show()
 # %%
     # Peak width (seconds)
-    widths = [c[4] / FPS for c in correlations]
-    average_peak_width = [w.mean() for w in widths]
-    peak_width_stdev = [p.std() for p in widths]
-    plt.errorbar(lengths, average_peak_width, peak_width_stdev, fmt=".")
-    plt.title("Average correlation peak width")
-    plt.xlabel("Board length (cm)")
-    plt.ylabel("Peak width (sec)")
-    plt.show()
+    # widths = [c[4] / FPS for c in correlations]
+    # average_peak_width = [w.mean() for w in widths]
+    # peak_width_stdev = [p.std() for p in widths]
+    # plt.errorbar(lengths, average_peak_width, peak_width_stdev, fmt=".")
+    # plt.title("Average correlation peak width")
+    # plt.xlabel("Board length (cm)")
+    # plt.ylabel("Peak width (sec)")
+    # plt.show()
 #%%
     # Plot blocked probabilities as function of board size
-    blocked_probabilities = [probability[0] for probability in occupied_probabilities]
-    plt.plot(lengths, blocked_probabilities, ".")
-    plt.title("Blocked probability as function of board length")
-    plt.xlabel("Board length (cm)")
-    plt.ylabel("Occupied probability")
-    plt.savefig("blocked_probabilities.png", bbox_inches='tight')
-    plt.show()
+    # blocked_probabilities = [probability[0] for probability in occupied_probabilities]
+    # plt.plot(lengths, blocked_probabilities, ".")
+    # plt.title("Blocked probability as function of board length")
+    # plt.xlabel("Board length (cm)")
+    # plt.ylabel("Occupied probability")
+    # plt.savefig("blocked_probabilities.png", bbox_inches='tight')
+    # plt.show()
 #%%
     # Merge results that belong to the same board size (multiple files)
     unique_lengths = np.unique(lengths)
@@ -102,26 +102,34 @@ if __name__ == "__main__":
     # plt.ylabel("Average duration (sec)")
     # plt.show()
 #%%
-   # Find correlations and peaks in correlations
-   #  fig = plt.figure(figsize=(30, 5))
-    lags, values, peaks, peak_values, peak_widths = autocorrelations(analysis_results[25])
+    # Find correlations and peaks in correlations
+    fig = plt.figure(figsize=(30, 5))
+    lags, values, peaks, peak_values, peak_widths = autocorrelations(analysis_results[0])
     # plt.ylim(-0.075, 0.075)
     plt.xlim(0, 150)
-    plt.title("Correlations for " + str(lengths[25]) + "cm")
+    plt.title("Correlations for " + str(lengths[0]) + "cm")
     plt.xlabel("Frame num")
     plt.ylabel("Autocorrelation (logscale)")
     # plt.semilogy()
     plt.show()
 #%%
-    first_decay_slope(lags, values, True)
+    # first_decay_slope(lags, values, True)
 # %%
-    # Fourier transform of correlations graph
-    xf = np.fft.fftfreq(len(values))
-    fourier = np.fft.fft(values)
-    plt.plot(xf, fourier, "-")
-    plt.xlim(-0.05, 0.05)
-    plt.xlabel("Frequency (1/frames)")
+    # Fourier transform of correlations graph. Sampling rate is 1/frames per second
+    xf = np.fft.fftshift(np.fft.fftfreq(len(values), d=1/FPS))
+    fourier = np.fft.fftshift(np.abs(np.fft.fft(values)))
+# %%
+    # envelope = scipy.signal.hilbert(np.abs(fourier))
+    envelope = scipy.signal.savgol_filter(np.abs(fourier), 401, 1)
+    env_peaks, properties = scipy.signal.find_peaks(envelope, prominence=1)
+    plt.plot(xf, fourier)
+    plt.plot(xf, envelope)
+    plt.plot(xf[env_peaks], envelope[env_peaks], "x")
+    plt.xlim(0, 1.75)
+    plt.xlabel("Frequency (Hz)")
     plt.ylabel("Autocorrelations Fourier transform")
-    plt.title("Fourier transform of autocorrelations plot for " + str(lengths[25]) + "cm")
+    plt.title("Fourier transform of autocorrelations plot for " + str(lengths[0]) + "cm")
     plt.show()
-# %%
+    diffs = np.diff(xf[env_peaks])
+    # print(diffs.mean(), diffs.std())
+    # %%
