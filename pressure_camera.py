0aed95ae13066daffadb7800a3b5c1a268c5f566
omerchor <65480691+omerchor@users.noreply.github.com>
2020-12-16 15:00:25 +0200

    Added errors on average

--- pressure_camera.py
+++ pressure_camera.py
@@ -1,6 +1,8 @@
+import collections
 import os
 import time
 from multiprocessing import Pool
+import logging
 
 import numpy as np
 import cv2
@@ -19,6 +21,10 @@ X_INDEX = 1
 videos_path = r"C:\Users\OmerChor\Pictures\Camera Roll"
 
 
+def power_law(B, x):
+    return B[0] + B[1]*x
+
+
 def analyze_frame(frame):
     # Crop frame [y, x]
 
@@ -26,8 +32,10 @@ def analyze_frame(frame):
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     # define range of blue color in HSV
-    barrier_color_lower = np.array([140, 150, 150])
-    barrier_color_upper = np.array([150, 255, 255])
+    # barrier_color_lower = np.array([140, 150, 150])
+    # barrier_color_upper = np.array([150, 255, 255])
+    barrier_color_lower = np.array([120, 180, 120])
+    barrier_color_upper = np.array([130, 255, 255])
 
     # Threshold the HSV image to get only blue colors
     mask = cv2.inRange(hsv, barrier_color_lower, barrier_color_upper)
@@ -40,7 +48,6 @@ def analyze_frame(frame):
 
 
 def analyze_video(path, show=False):
-    print("Analyzing", path)
     cap = cv2.VideoCapture(path)
     fps = cap.get(cv2.CAP_PROP_FPS)
     widths = []
@@ -54,10 +61,14 @@ def analyze_video(path, show=False):
 
             # Crop frame [y, x]
             frame = frame[:230, 150:500]
-
-            arena_width, barrier_position, mask = analyze_frame(frame)
+            try:
+                arena_width, barrier_position, mask = analyze_frame(frame)
+            # This happens due to the mask finding no pixels at all. Might happen in a few frames
+            except ValueError:
+                print(f"--> Empty frame in {os.path.basename(path)}")
+                continue
             # Ignore very large barrier movements - they are probably just a mistake
-            if len(widths) > 1 and widths[-1] - arena_width > 20:
+            if len(widths) > 1 and (widths[-1] - arena_width > 5 or widths[-1] - arena_width < -5):
                 continue
             widths.append(arena_width)
 
@@ -76,20 +87,24 @@ def analyze_video(path, show=False):
                 k = cv2.waitKey(5) & 0xFF
                 if k == 27:
                     break
+    except Exception:
+        print(f"Error in {os.path.basename(path)}")
+        raise
     finally:
         cap.release()
         cv2.destroyAllWindows()
 
+    print(f"{os.path.basename(path)}: {len(widths) / fps:.2f} sec")
     return np.arange(0, len(widths)) / fps, widths
 
 
 def main():
-    # analyze_video(os.path.join(r"C:\Users\OmerChor\Pictures\Camera Roll",
-    #                            "WIN_20201209_16_09_58_Pro.mp4"),
+    files = [os.path.join(r"C:\Users\OmerChor\Pictures\Camera Roll", f) for f in os.listdir(videos_path) if f.endswith(".mp4")]
+    # analyze_video(r"C:\Users\OmerChor\Pictures\Camera Roll\WIN_20201216_12_09_41_Pro.mp4",
     #               True)
     # return
-
-    files = [os.path.join(r"C:\Users\OmerChor\Pictures\Camera Roll", f) for f in os.listdir(videos_path) if f.endswith(".mp4")]
+    #
+    #
     # results = []
     with Pool(8) as p:
         t = time.time()
@@ -104,32 +119,71 @@ def main():
             min_num_of_frames = len(times)
             min_times = times
 
-    # for i, f in enumerate(files):
-    #     start_time = time.time()
-    #     print(f"{i+1}/{len(files)}: Analyzing {os.path.split(f)[-1]}...",)
-    #     times, widths = analyze_video(f)
-    #     results.append((times, widths))
-    #     if len(times) < min_num_of_frames:
-    #         min_num_of_frames = len(times)
-    #         min_times = times
-    #     print(f"Elapsed {time.time() - start_time:.2f} seconds")
-
+    # This dict maps arena width to time
+    widths_dict = collections.defaultdict(list)
     for times, widths in results:
         plt.plot(times, widths)
 
+        # Find inverse (time as function of width)
+        for i, w in enumerate(widths):
+            widths_dict[w].append(times[i])
+
+    # The error the time of each width is statistical error of times with same width
+    time_errs_by_width = {width: np.std(times) / np.sqrt(len(times) - 1) for width, times in widths_dict.items()
+                          if len(times) > 1}
+
     # Compute average
     averages = np.zeros(min_num_of_frames)
+    averages_errs = np.zeros(min_num_of_frames)
+    time_errs = np.zeros(min_num_of_frames)
     for t in range(min_num_of_frames):
         widths_at_time = [r[1][t] for r in results]
         averages[t] = np.average(widths_at_time)
-    plt.plot(min_times, averages, label="Average", color="black")
+        averages_errs[t] = np.std(widths_at_time) / np.sqrt(len(widths_at_time) - 1)
+        try:
+            rounded_width = round(averages[t])
+            time_errs[t] = time_errs_by_width[rounded_width]
+        # If the average value was not measured, find error from neighbours
+        except KeyError:
+            err_before = None
+            err_after = None
+            difference = 0
+            while err_before is None and err_after is None:
+                difference += 1
+                if rounded_width - difference in time_errs_by_width:
+                    err_before = time_errs_by_width[rounded_width - difference]
+                if rounded_width + difference in time_errs_by_width:
+                    err_after = time_errs_by_width[rounded_width - difference]
+            if err_before is not None and err_after is not None:
+                time_errs[t] = (err_before + err_after) / 2
+            elif err_before is not None:
+                time_errs[t] = err_before
+            elif err_after is not None:
+                time_errs[t] = err_after
+    plt.plot(min_times, averages,
+             label="Average", color="black")
+    plt.fill_between(min_times, averages + averages_errs, averages - averages_errs)
+    plt.fill_betweenx(averages, min_times + time_errs, min_times - time_errs)
 
     plt.xlabel("Time [sec]")
     plt.ylabel("Arena width [pixels]")
-    plt.title(f"Free expansion of {len(files)} hexbugs")
+    plt.title(f"Free expansion of hexbugs (ensemble size: {len(files)})")
     plt.legend()
     plt.show()
 
+    plt.plot(min_times, averages,
+             label="Average", color="black")
+    plt.fill_between(min_times, averages + averages_errs, averages - averages_errs)
+    plt.fill_betweenx(averages, min_times + time_errs, min_times - time_errs)
+    plt.show()
+
+    # diffs = [np.diff(w) for t, w in results]
+    # plt.hist(diffs, bins=np.arange(-2.5, 7.5))
+    # plt.xlabel("Step size")
+    # plt.ylim(0, 500)
+    # plt.show()
+
 
 if __name__ == "__main__":
+    plt.clf()
     main()
